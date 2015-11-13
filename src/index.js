3bffd8e8d03dba97e8d60f445513b3be72902e0e
Aleksey Kulikov <alekseys.kulikov@gmail.com>
2015-11-13 14:22:10 +0100

    rewrite with es6 + use `is-plain-obj` as deps

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import isPlainObject from 'is-plain-obj'
 
 /**
  * Parse `opts` to valid IDBKeyRange.
@@ -7,16 +8,16 @@
  * @return {IDBKeyRange}
  */
 
-module.exports = function range(opts) {
-  var IDBKeyRange = global.IDBKeyRange || global.webkitIDBKeyRange
+export default function range(opts) {
+  const IDBKeyRange = global.IDBKeyRange || global.webkitIDBKeyRange
   if (opts instanceof IDBKeyRange) return opts
   if (typeof opts === 'undefined') return null
-  if (!isObject(opts)) return IDBKeyRange.only(opts)
-  var keys = Object.keys(opts).sort()
+  if (!isPlainObject(opts)) return IDBKeyRange.only(opts)
+  const keys = Object.keys(opts).sort()
 
-  if (keys.length == 1) {
-    var key = keys[0]
-    var val = opts[key]
+  if (keys.length === 1) {
+    const key = keys[0]
+    const val = opts[key]
 
     switch (key) {
       case 'eq': return IDBKeyRange.only(val)
@@ -24,30 +25,19 @@ module.exports = function range(opts) {
       case 'lt': return IDBKeyRange.upperBound(val, true)
       case 'gte': return IDBKeyRange.lowerBound(val)
       case 'lte': return IDBKeyRange.upperBound(val)
-      default: throw new TypeError('`' + key + '` is not valid key')
+      default: throw new TypeError(`"${key}" is not valid key`)
     }
   } else {
-    var x = opts[keys[0]]
-    var y = opts[keys[1]]
-    var pattern = keys.join('-')
+    const x = opts[keys[0]]
+    const y = opts[keys[1]]
+    const pattern = keys.join('-')
 
     switch (pattern) {
       case 'gt-lt': return IDBKeyRange.bound(x, y, true, true)
       case 'gt-lte': return IDBKeyRange.bound(x, y, true, false)
       case 'gte-lt': return IDBKeyRange.bound(x, y, false, true)
       case 'gte-lte': return IDBKeyRange.bound(x, y, false, false)
-      default: throw new TypeError('`' + pattern +'` are conflicted keys')
+      default: throw new TypeError(`"${pattern}" are conflicted keys`)
     }
   }
-}
-
-/**
- * Check if `obj` is an object (an even not an array).
- *
- * @param {Object} obj
- * @return {Boolean}
- */
-
-function isObject(obj) {
-  return Object.prototype.toString.call(obj) == '[object Object]'
 }
--- test/index.js
+++ test/index.js
@@ -1,100 +1,101 @@
-require('treo-websql').polyfill()
-var expect = require('chai').expect
-var range = require('../lib')
+import treoWebsql from 'treo-websql'
+import { expect } from 'chai'
+import range from '../src'
 
-describe('idb-range', function() {
-  var x = '1'
-  var y = '2'
-  var z = ['3', '4']
-  var val
+treoWebsql.polyfill()
+describe('idb-range', () => {
+  const x = '1'
+  const y = '2'
+  const z = ['3', '4']
+  let val
 
-  it('validates arguments', function() {
-    expect(function() { range({ g: 2 }) }).throw('`g` is not valid key')
-    expect(function() { range({ gt: 2, gte: 5 }) }).throw('conflicted keys')
-    expect(function() { range({ lt: 'a', gte: 'ba', lte: 'c' }) }).throw('conflicted keys')
+  it('validates arguments', () => {
+    expect(() => { range({ g: 2 }) }).throw('`g` is not valid key')
+    expect(() => { range({ gt: 2, gte: 5 }) }).throw('conflicted keys')
+    expect(() => { range({ lt: 'a', gte: 'ba', lte: 'c' }) }).throw('conflicted keys')
   })
 
-  it('all keys <= x', function() {
+  it('all keys <= x', () => {
     val = range({ lte: x })
-    expect(val.upperOpen).false
-    expect(val.lowerOpen).true
+    expect(val.upperOpen).equal(false)
+    expect(val.lowerOpen).equal(true)
     expect(val.upper).equal(x)
-    expect(val.lower).undefined
+    expect(val.lower).equal(undefined)
   })
 
-  it('all keys < x', function() {
+  it('all keys < x', () => {
     val = range({ lt: x })
-    expect(val.upperOpen).true
-    expect(val.lowerOpen).true
+    expect(val.upperOpen).equal(true)
+    expect(val.lowerOpen).equal(true)
     expect(val.upper).equal(x)
-    expect(val.lower).undefined
+    expect(val.lower).equal(undefined)
   })
 
-  it('all keys >= y', function() {
+  it('all keys >= y', () => {
     val = range({ gte: y })
-    expect(val.upperOpen).true
-    expect(val.lowerOpen).false
-    expect(val.upper).undefined
+    expect(val.upperOpen).equal(true)
+    expect(val.lowerOpen).equal(false)
+    expect(val.upper).equal(undefined)
     expect(val.lower).equal(y)
   })
 
-  it('all keys > y', function() {
+  it('all keys > y', () => {
     val = range({ gt: y })
-    expect(val.upperOpen).true
-    expect(val.lowerOpen).true
-    expect(val.upper).undefined
+    expect(val.upperOpen).equal(true)
+    expect(val.lowerOpen).equal(true)
+    expect(val.upper).equal(undefined)
     expect(val.lower).equal(y)
   })
 
-  it('all keys >= x && <= y', function() {
+  it('all keys >= x && <= y', () => {
     val = range({ lte: y, gte: x })
-    expect(val.upperOpen).false
-    expect(val.lowerOpen).false
+    expect(val.upperOpen).equal(false)
+    expect(val.lowerOpen).equal(false)
     expect(val.upper).equal(y)
     expect(val.lower).equal(x)
   })
 
-  it('all keys > x && < y', function() {
+  it('all keys > x && < y', () => {
     val = range({ gt: x, lt: y })
-    expect(val.upperOpen).true
-    expect(val.lowerOpen).true
+    expect(val.upperOpen).equal(true)
+    expect(val.lowerOpen).equal(true)
     expect(val.upper).equal(y)
     expect(val.lower).equal(x)
   })
 
-  it('all keys > x && <= y', function() {
+  it('all keys > x && <= y', () => {
     val = range({ gt: x, lte: y })
-    expect(val.upperOpen).false
-    expect(val.lowerOpen).true
+    expect(val.upperOpen).equal(false)
+    expect(val.lowerOpen).equal(true)
     expect(val.upper).equal(y)
     expect(val.lower).equal(x)
   })
 
-  it('all keys >= x && < y', function() {
+  it('all keys >= x && < y', () => {
     val = range({ lt: y, gte: x })
-    expect(val.upperOpen).true
-    expect(val.lowerOpen).false
+    expect(val.upperOpen).equal(true)
+    expect(val.lowerOpen).equal(false)
     expect(val.upper).equal(y)
     expect(val.lower).equal(x)
   })
 
-  it('the key = z', function() {
+  it('the key = z', () => {
     val = range({ eq: z })
-    expect(val.upperOpen).false
-    expect(val.lowerOpen).false
+    expect(val.upperOpen).equal(false)
+    expect(val.lowerOpen).equal(false)
     expect(val.upper).eql(z)
     expect(val.lower).eql(z)
   })
 
-  it('allows any value', function() {
+  it('allows any value', () => {
     val = range(z)
-    expect(val.upperOpen).false
-    expect(val.lowerOpen).false
+    expect(val.upperOpen).equal(false)
+    expect(val.lowerOpen).equal(false)
     expect(val.upper).eql(z)
     expect(val.lower).eql(z)
   })
 
-  it('returns nothing when key is undefined', function() {
-    expect(range()).null
+  it('returns nothing when key is undefined', () => {
+    expect(range()).equal(null)
   })
 })
